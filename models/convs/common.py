27dbe6e5c78af7d3ab9939ad38cc181a4cf7c829
JusticeV452 <55002917+JusticeV452@users.noreply.github.com>
2025-07-22 12:23:58 -0400

    Initial ConvLayer implementation

---
+++ models/convs/common.py
@@ -0,0 +1,131 @@
+
+import torch
+import torch.nn.functional as F
+
+from pydantic import Field
+from torch import nn
+from torch.utils.checkpoint import checkpoint_sequential
+from typing import Optional, Tuple, Type, Union
+
+from components.common import SEBlock
+from models.convs.dynamic import FILTER_TYPES, DynamicConv2d_v2
+from serialization import SerializableModule, Lambda, AutoLambda
+
+
+class ConvLayer(SerializableModule):
+    in_channels: int = Field(..., description="Number of input channels")
+    out_channels: int = Field(..., description="Number of output channels")
+    mid_channels: Optional[int] = Field(
+        None, description="Intermediate channel size (defaults to out_channels if None)"
+    )
+    kernel_size: int = Field(3, description="Kernel size for convolutions")
+    num_convs: int = Field(2, description="Number of convolution layers")
+    norm_layer: Optional[AutoLambda[Tuple[int], nn.Module]] = Field(
+        nn.BatchNorm2d, description="Normalization layer class"
+    )
+    activ_type: AutoLambda[Tuple[()], nn.Module] = Field(
+        default_factory=Lambda(nn.ReLU, inplace=True),
+        description="Activation function factory"
+    )
+    conv_type: Type[nn.Module] = Field(
+        nn.Conv2d, description="Convolution layer class"
+    )
+    residual: Optional[Union[str, dict, float, int, AutoLambda[Tuple[()], torch.Tensor]]] = Field(
+        None, description="Residual connection type or configuration"
+    )
+    use_bias: bool = Field(False, description="Whether to use bias in convolutions")
+    use_checkpointing: bool = Field(False, description="Enable checkpointing for convolutions")
+    filter_gen: Optional[Type[nn.Module]] = Field(
+        None, description="Filter generator class used if dynamic convultion is enabled"
+    )
+    se_param: Optional[Union[int, AutoLambda[Tuple[()], nn.Module]]] = Field(
+        None, description="Int: acts as reduction_ratio for SEBlock, func: factory for se block"
+    )
+
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super().__init__(in_channels=in_channels, out_channels=out_channels, **kwargs)
+        # residual = None if in_channels != out_channels else residual
+        residual = self.residual
+        in_channels = self.in_channels
+        out_channels = self.out_channels
+        num_convs = self.num_convs
+        filter_gen = self.filter_gen
+
+        if filter_gen:
+            filter_gen = FILTER_TYPES[filter_gen] if isinstance(filter_gen, str) else filter_gen
+            self.conv_type = Lambda(DynamicConv2d_v2, filt_gen=filter_gen)
+
+        self.residual_transform = None
+        self.residual_type = None
+        if callable(residual):
+            self.residual_type = "callable"
+            self.residual = nn.Parameter(residual())
+            self.residual_transform = lambda result, inp: result + self.residual * inp
+        elif type(residual) is dict:
+            self.residual_type = "conv"
+            self.residual = nn.Conv2d(
+                in_channels=in_channels, out_channels=out_channels, **residual)
+            self.residual_transform = lambda result, inp: result + self.residual(inp)
+        elif type(residual) in [float, int]:
+            self.residual_type = "constant"
+            self.residual = nn.Parameter(torch.full((1,), residual)[0])
+            self.residual_transform = lambda result, inp: result + self.residual * inp
+        elif "pad" in residual:
+            self.residual_type = residual
+            self.residual = nn.Parameter(torch.ones(in_channels))
+            self.residual_transform = self.residual_pad
+        if not mid_channels or num_convs == 1:
+            mid_channels = out_channels
+
+        def sub_layer(in_channels, out_channels):
+            layer = [self.conv_type(
+                in_channels, out_channels, kernel_size=self.kernel_size,
+                padding=(self.kernel_size - 1) // 2, bias=self.use_bias
+            )]
+            if self.norm_layer:
+                layer.append(self.norm_layer(out_channels))
+            layer.append(self.activ_type())
+            return layer
+
+        convs = sub_layer(in_channels, mid_channels)
+        for i in range(num_convs - 2):
+            convs.extend(sub_layer(mid_channels, mid_channels))
+        if num_convs > 1:
+            convs.extend(sub_layer(mid_channels, out_channels))
+        self.convs = nn.Sequential(*convs,)
+        self.se_block = None
+        se_param = self.se_param
+        if type(se_param) is int:
+            self.se_block = SEBlock(out_channels, reduction_ratio=se_param)
+        elif callable(se_param):
+            self.se_block = se_param()
+
+    def residual_pad(self, result, inp):
+        res = self.residual
+        if "sigmoid" in self.residual_type:
+            res = nn.Sigmoid()(res)
+        inp = res.unsqueeze(0).unsqueeze(2).unsqueeze(2) * inp
+        num_dims = len(result.shape)
+        for i in range(1, num_dims):
+            if inp.shape[i] > result.shape[i]:
+                indicies = torch.tensor(
+                    range(0, result.shape[i]),
+                    device=inp.device
+                )
+                inp = torch.index_select(inp, i, indicies)
+            elif inp.shape[i] < result.shape[i]:
+                diff = result.shape[i] - inp.shape[i]
+                remaining = diff % 2
+                padding = (num_dims - i - 1) * [0, 0] + [diff // 2, diff // 2 + remaining] + (i - 1) * [0, 0]
+                inp = F.pad(inp, padding)
+        return result + inp
+
+    def forward(self, x):
+        convs = (
+            (lambda inp: checkpoint_sequential(self.convs, self.num_convs - 1, inp))
+            if self.use_checkpointing and self.num_convs > 1 else self.convs
+        )
+        result = convs(x)
+        if type(self.residual_transform) is not type(None):
+            result = self.residual_transform(result, x)
+        return result
