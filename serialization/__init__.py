3a3d754c9e2d1cd240b43261e2c94680ad1cc86b
JusticeV452 <55002917+JusticeV452@users.noreply.github.com>
2025-08-07 15:47:25 -0400

    Added SerializableModel to serialization root

--- serialization/__init__.py
+++ serialization/__init__.py
@@ -1,5 +1,5 @@
 from .core import ParamManager
 from .lambda_ import AutoLambda, Lambda, LambdaModuleT
-from .nn import SerializableModule
+from .nn import SerializableModule, SerializableModel
 
-__all__ = ["ParamManager", "AutoLambda", "Lambda", "LambdaModuleT", "SerializableModule"]
+__all__ = ["ParamManager", "AutoLambda", "Lambda", "LambdaModuleT", "SerializableModule", "SerializableModel"]
